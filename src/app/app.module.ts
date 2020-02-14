28d1242546e622818abd82e98d3952da5725a06d
Andrew <andrewhinkson88@gmail.com>
2020-02-14 17:05:31 -0500

    Added JQuery Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,15 +17,16 @@ import {
 
 import {EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { TOASTR_TOKEN, Toastr } from './common/toastr.service';
+
+import {JQ_TOKEN, TOASTR_TOKEN,Toastr, CollapsibleWellComponent } from './common/index'
 import { appRoutes } from './routes';
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CollapsibleWellComponent } from './common/collapsible-well.component';
 
-declare let toastr: Toastr;
 
+ let toastr: Toastr = window['toastr'];
+ let jQuery = window['$'];
 
 @NgModule({
 
--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {AuthService} from '../user/auth.service'
+import { ISession, EventService } from '../events';
 @Component({
   selector: 'nav-bar',
   templateUrl: './navbar.component.html',
@@ -11,7 +12,15 @@ import {AuthService} from '../user/auth.service'
   `]
 })
 export class NavBarComponent {
-constructor (private auth: AuthService){
+  searchTerm: string = "";
+  foundSessions: ISession[];
+constructor (private auth: AuthService, private eventService: EventService){
 
+}
+searchSessions(searchTerm){
+  this.eventService.searchSessions(searchTerm).subscribe(sessions => {
+    this.foundSessions = sessions;
+    console.log(this.foundSessions);
+  })
 }
 }
