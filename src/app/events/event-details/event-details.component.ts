c0ceea78771236db4cb939bb01d3844d8e51af87
Andrew <andrewhinkson88@gmail.com>
2020-02-17 01:51:36 -0500

    Added code so the modal closes after choosing an option

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {EventService} from '../shared/event.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 import { IEvent, ISession } from '../shared';
 @Component({
 
@@ -23,7 +23,12 @@ constructor (private eventService: EventService, private route: ActivatedRoute) 
 
 }
 ngOnInit () {
-this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+
+  this.route.params.forEach((params: Params) =>{
+    this.event = this.eventService.getEvent(+params['id']);
+    this.addMode = false;
+  })
+
 }
 
 addSession(){
